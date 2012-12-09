9b30af03307e84d2ea547904eac55e3524dc3173
Rex Feng <rexfeng@gmail.com>
2012-12-09 05:07:10 -0500

    modify to show/log box on mouse down move and up

--- assets/js/lib/screenboxer.js
+++ assets/js/lib/screenboxer.js
@@ -3,32 +3,82 @@ $(document).ready(function() {
 (function screen_boxer(){
 
     
-      var boxer_area = $(window);
-    
-      var window_width = boxer_area.width();
-      var window_height = boxer_area.height();
-    
-      $('#info').html("Width: " + window_width + " Height: " + window_height);
-    
-      boxer_area.mousemove(function(e) {
-        $('#test').html("x-coord: " + e.pageX + "<br /> y-coord: " + e.pageY);
+  var boxer_area = $(window);
+
+  // get x y total width and height for screen
+    // var window_width = boxer_area.width();
+    // var window_height = boxer_area.height();
+
+    // $('#info').html("Width: " + window_width + " Height: " + window_height);
+
+  // get current x y mouse coord
+    // boxer_area.mousemove(function(e) {
+    //   $('#test').html("x-coord: " + e.pageX + "<br /> y-coord: " + e.pageY);
+    // });
+
+  // track clicks
+    // var log = "";
+
+    // boxer_area.on('click', function(e) {
+    //   log += "x: " + e.pageX + " y: " + e.pageY + "<br />";
+    //   $('#log').html(log);
+    // });  
+  
+  // when mousedown -> drag -> mouseoff events, draw box
+
+    // boxer_area.mouseup(function(e){
+    //   $('#log').append(" Mouseup: " + e.pageX + ", " + e.pageY + "<br />");
+    // }).mousedown(function(e){
+    //   $('#log').append(" Mousedown: " + e.pageX + ", " + e.pageY);
+    // });
+
+  // check if elements are in. how greedy selection?
+
+
+
+  var xbox, ybox;
+
+  boxer_area.mousedown(function(e){
+
+    xbox = e.pageX;
+    ybox = e.pageY;
+
+    $("#boxer").attr({ id: '' });
+    box = $('<div style="border:1px #333 solid;position:fixed;">').hide();
+    $(document.body).append(box);
+    box.attr({id: 'boxer'}).css({ top: e.pageY, left: e.pageX }).fadeIn();
+
+  });
+
+  boxer_area.mousemove(function(e) {
+
+    if (e.pageX > xbox) {
+      $("#boxer").css({ width: Math.abs(e.pageX - xbox) });
+    } else {
+      $("#boxer").css({ left: e.pageX, right: xbox, width: Math.abs(e.pageX - xbox)
       });
-    
-      var log = "";
-    
-      boxer_area.on('click', function(e) {
-        log += "x: " + e.pageX + " y: " + e.pageY + "<br />";
-        $('#log').html(log);
-      });  
-    
-  // tasks
-  
-    // get x y total width and height for screen
-  
-    // when onclick -> drag -> offclick events, draw box
-    // check if elements are in
-  
-    // how greedy selection?
+    }
+
+    if (e.pageY > ybox) {
+      $("#boxer").css({ height: Math.abs(e.pageY - ybox) });
+    } else {
+      $("#boxer").css({ top: e.pageY, bottom: ybox, height: Math.abs(e.pageY - ybox) });
+    }
+
+  });
+
+  boxer_area.mouseup(function() {
+    console.log("Top: " + $('#boxer').css('top'));
+    console.log("Bottom: " + $('#boxer').css('bottom'));
+    console.log("Left: " + $('#boxer').css('left'));
+    console.log("Right: " + $('#boxer').css('right'));
+    console.log("Width: " + $('#boxer').css('width'));
+    console.log("Height: " + $('#boxer').css('height'));
+    $("#boxer").attr({ id: '' })
+  });
+
+
+
 
 
 
